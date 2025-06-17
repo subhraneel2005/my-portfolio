7936b7c43293a02845aee941c172021b167aea68
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-06-18 00:00:11 +0530

    Refactor layout and add developer attribution section in App component

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,23 @@ export default function App() {
   const words = headline.split(" ");
   const totalHeadlineDelay = 0.2 * words.length;
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center space-y-4 py-20">
+    <div className="min-h-screen w-full flex flex-col items-center justify-center space-y-4 pt-16 pb-4">
       <Intro words={words} totalHeadlineDelay={totalHeadlineDelay} />
       <TechStack totalHeadlineDelay={totalHeadlineDelay} />
       <WorkExperience totalHeadlineDelay={totalHeadlineDelay} />
       <Github totalHeadlineDelay={totalHeadlineDelay} />
       <Project totalHeadlineDelay={totalHeadlineDelay} />
+
+      <div className="w-full mt-12 dark:text-white/80 bottom-2 text-black/80 flex justify-center items-center max-w-3xl px-4">
+        Developed with ❤️ by{" "}
+        <a
+          className="cursor-pointer ml-1"
+          href="https://x.com/Subhraneel55545"
+          target="_blank"
+        >
+          Subhraneel
+        </a>
+      </div>
     </div>
   );
 }
